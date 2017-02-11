e25267285cdeb6be4fd6c6a7f2abd9d627911da0
John P. Reardon <jp@jpreardon.com>
2017-02-11 13:35:56 -0500

    Added an alternate data transformation, and translate UTC to local time. Grabbed some more recent data.

--- data-transform.py
+++ data-transform.py
@@ -4,6 +4,36 @@
 # the day, and a column for each day
 
 import sys
+from datetime import datetime
+from dateutil import tz
+
+def find_datetime(date_time, dataset):
+    for i, line in enumerate(dataset):
+        if date_time == line[0]:
+            return i
+
+def convert_utc_to_local(utc_date_time):
+    from_zone = tz.gettz('UTC')
+    to_zone = tz.gettz('America/New_York')
+
+    utc = datetime.strptime(utc_date_time, '%Y-%m-%d %H:%M:%S')
+
+    # Tell the datetime object that it's in UTC time zone since
+    # datetime objects are 'naive' by default
+    utc = utc.replace(tzinfo=from_zone)
+
+    # Convert time zone
+    local_date_time = utc.astimezone(to_zone)
+
+    # Convert it to a string in a format we expect
+    local_date_time_str = local_date_time.strftime('%Y-%m-%d %H:%M:%S')
+
+    return local_date_time_str
+
+# Output format
+TIME_ROWS = False
+if sys.argv[3] == "1":
+    TIME_ROWS = True
 
 # Load the data
 log_data = open(sys.argv[1], 'r')
@@ -15,24 +45,65 @@ data = []
 days = []
 
 for line in log_data:
-    date = line.split(",")[0].split(" ")[0]
+    date = line.split(",")[0]
+    if date != "report_datetime":
+        date = convert_utc_to_local(date).split(" ")[0]
     if len(days) == 0 or days[len(days) - 1] != date:
         days.append(date)
 
-for day in days:
-    datarow = [day]
-    for hour in range(24):
-        if day == "report_datetime":
-            # First row is a header
-            datarow.append(str(hour).zfill(2) + ":00")
-            datarow.append(str(hour).zfill(2) + ":15")
-            datarow.append(str(hour).zfill(2) + ":30")
-            datarow.append(str(hour).zfill(2) + ":45")
-        else:
-            # Fill the rest of the array with zeros
-            for quarter in range(4):
-                datarow.append(0)
+# Create the array (with dates as rows)
+if TIME_ROWS != True:
+
+    for day in days:
+        datarow = [day]
+        for hour in range(24):
+            if day == "report_datetime":
+                # First row is a header
+                datarow.append(str(hour).zfill(2) + ":00")
+                datarow.append(str(hour).zfill(2) + ":15")
+                datarow.append(str(hour).zfill(2) + ":30")
+                datarow.append(str(hour).zfill(2) + ":45")
+            else:
+                # Fill the rest of the array with zeros
+                for quarter in range(4):
+                    datarow.append(0)
+        data.append(datarow)
+
+# Create the array (with times as rows)
+if TIME_ROWS:
+
+    # First row
+    datarow = []
+    for day in days:
+        datarow.append(day)
+
     data.append(datarow)
+
+    # First column is a time, the rest should be zeros
+    # Lots of repitition here, sad!
+    for hour in range(24):
+        for hourpart in range(4):
+            if hourpart == 0:
+                datarow = [str(hour).zfill(2) + ":00"]
+                for day in days:
+                    if day != "report_datetime":
+                        datarow.append(0)
+            if hourpart == 1:
+                datarow = [str(hour).zfill(2) + ":15"]
+                for day in days:
+                    if day != "report_datetime":
+                        datarow.append(0)
+            if hourpart == 2:
+                datarow = [str(hour).zfill(2) + ":30"]
+                for day in days:
+                    if day != "report_datetime":
+                        datarow.append(0)
+            if hourpart == 3:
+                datarow = [str(hour).zfill(2) + ":45"]
+                for day in days:
+                    if day != "report_datetime":
+                        datarow.append(0)
+            data.append(datarow)
 
 log_data.seek(0,0)
 
@@ -41,10 +112,15 @@ row = 0
 col = 0
 for line in log_data:
     if line.split(",")[0].split(" ")[0] != "report_datetime":
-        date = line.split(",")[0].split(" ")[0]
-        time = line.split(",")[0].split(" ")[1][:-3]
-        row = days.index(date)
-        col = data[0].index(time)
+        localtime = str(convert_utc_to_local(line.split(",")[0]))
+        date = localtime.split(" ")[0]
+        time = localtime.split(" ")[1][:-3]
+        if TIME_ROWS:
+            row = find_datetime(time, data)
+            col = data[0].index(date)
+        else:
+            row = days.index(date)
+            col = data[0].index(time)
         data[row][col] = int(line.split(",")[2])
 
 # Write array to a file
